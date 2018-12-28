0f5e69b4d4d3971e47eebce711262a5db9f3fc02
Kenneth Phang <bunnyppl@gmail.com>
2018-12-28 17:17:24 +0800

    fixes

--- src/components/Page/Header/LangDialog.js
+++ src/components/Page/Header/LangDialog.js
@@ -1,63 +1,94 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-//import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-//import DialogActions from '@material-ui/core/DialogActions';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Avatar from '@material-ui/core/Avatar';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import ListItemText from '@material-ui/core/ListItemText';
-//import PersonIcon from '@material-ui/icons/Person';
-//import { withStyles } from '@material-ui/core/styles';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import PropTypes from "prop-types";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Avatar from "@material-ui/core/Avatar";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "@material-ui/core/Button";
+import { withStyles } from "@material-ui/core/styles";
+import blue from "@material-ui/core/colors/blue";
+import DialogContent from "@material-ui/core/DialogContent";
+const languages = ["English", "中文"];
 
-const languages = ['English', '中文'];
-
+const styles = {
+  avatar: {
+    backgroundColor: blue[100],
+    color: blue[600]
+  }
+};
 
 class LanguageDialog extends React.Component {
-
   state = {
     open: false,
+    selectedValue: "English"
   };
 
+  componentDidMount() {
+    //setInterval(this.open, 1000);
+  }
+
   handleClose = () => {
-    this.props.onClose(this.props.selectedValue);
+    this.setState({ open: false });
   };
 
   handleListItemClick = value => {
-    //this.state.open = false;
+    this.setState({
+      open: false
+    });
     this.props.onClose(value);
   };
 
+  handleLanguageMenuOpen = event => {
+    console.log(">>> lang menu open");
+    this.setState({ open: true });
+  };
+
   render() {
-    const { fullScreen, openLangDialog, classes } = this.props;
-    console.log(openLangDialog);
-    console.log(fullScreen);
+    const { classes, ...other } = this.props;
     return (
       <div>
-        <Dialog
-          fullScreen={fullScreen}
-          open={openLangDialog}
-          onClose={this.handleClose} 
-          aria-labelledby="responsive-dialog-title"
+        <Button
+          size="small"
+          variant="outlined"
+          color="primary"
+          onClick={this.handleLanguageMenuOpen}
         >
-          <DialogTitle id="responsive-dialog-title">{"Change language ?"}</DialogTitle>
-          <div>
-          <List>
-            {languages.map(lang => (
-              <ListItem button onClick={() => this.handleListItemClick(lang)} key={lang}>
-                <ListItemAvatar>
-                  <Avatar className={classes.avatar}>
-                    <FontAwesomeIcon icon="language" size="2x" />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={lang} />
-              </ListItem>
-            ))}
-          </List>
-          </div>
+          <FontAwesomeIcon icon="language" size="2x" />
+          &nbsp;Language &nbsp;{this.state.selectedValue}
+        </Button>
+        <Dialog
+          fullScreen
+          open={this.state.open}
+          aria-labelledby="confirmation-dialog-title"
+          {...other}
+        >
+          <DialogTitle id="confirmation-dialog-title">
+            {"Change language ?"}
+          </DialogTitle>
+          <DialogContent>
+            <div>
+              <List>
+                {languages.map(lang => (
+                  <ListItem
+                    button
+                    onClick={() => this.handleListItemClick(lang)}
+                    key={lang}
+                  >
+                    <ListItemAvatar>
+                      <Avatar className={classes.avatar}>
+                        <FontAwesomeIcon icon="language" size="2x" />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={lang} />
+                  </ListItem>
+                ))}
+              </List>
+            </div>
+          </DialogContent>
         </Dialog>
       </div>
     );
@@ -65,10 +96,8 @@ class LanguageDialog extends React.Component {
 }
 
 LanguageDialog.propTypes = {
-  fullScreen: PropTypes.bool.isRequired,
-  classes: PropTypes.object.isRequired,
-  onClose: PropTypes.func,
-  selectedValue: PropTypes.string,
+  classes: PropTypes.object.isRequired
 };
 
-export default LanguageDialog;
+const SimpleDialogWrapped = withStyles(styles)(LanguageDialog);
+export default SimpleDialogWrapped;
--- src/components/Page/Header/UserMenuItems.js
+++ src/components/Page/Header/UserMenuItems.js
@@ -1,65 +1,52 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import ImageAvatars from '../../Avatar'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import ImageAvatars from "../../Avatar";
 //import PropTypes from 'prop-types'
-import Button from '@material-ui/core/Button';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import LanguageDialog from './LangDialog';
+import Button from "@material-ui/core/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SimpleDialogWrapped from "./LangDialog";
 
 class UserMenuItems extends React.Component {
-    state = {
-        openLanguageDialog: false,
-        selectedValue: "English"
-    };
+  state = {};
 
-    handleLanguageMenuOpen = event => {
-        console.log("lang menu open");
-        this.setState({ openLanguageDialog: true });
-    };
-
-    handleClose = value => {
-        this.setState({ selectedValue: value, openLanguageDialog: false });
-    };
-
-    //<ImageAvatars src={user.image} className="user-pic" alt="" /> {user.username}
-    render() {
-        const { userA, anchorEl, isMenuOpen, menuClose, classes} = this.props;
-        return (
-            
-            <Menu
-              anchorEl={anchorEl}
-              anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-              transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-              open={isMenuOpen}
-              onClose={menuClose}
-            >
-                <MenuItem>
-                <ImageAvatars src={userA.image} className="user-pic" alt="" />{userA.email}
-                </MenuItem>
-                <MenuItem onClick={menuClose}>
-                    <NavLink to="/profile" exact className="nav-link">
-                        <Button size="small" className={classes.margin}>
-                            <FontAwesomeIcon icon="user" size="2x" />&nbsp;My Account
-                        </Button>
-                    </NavLink>
-                </MenuItem>
-                <MenuItem onClick={menuClose}>
-                    <Button size="small" className={classes.margin} onClick={this.handleLanguageMenuOpen}>
-                        <FontAwesomeIcon icon="language" size="2x" />&nbsp;Language ({this.state.selectedValue})
-                    </Button> 
-                    <LanguageDialog onClose={this.handleClose} openLangDialog={this.state.openLanguageDialog} fullScreen={false} classes={classes} selectedValue={this.state.selectedValue}></LanguageDialog>
-                </MenuItem>
-                <MenuItem onClick={menuClose}><NavLink to="/logout" exact className="nav-link">
-                    <Button size="small" className={classes.margin}>
-                    <FontAwesomeIcon icon="sign-out-alt" size="2x" />&nbsp;Logout
-                    </Button></NavLink>
-                </MenuItem>
-            </Menu>
-            
-        );
-    }
+  render() {
+    const { userA, anchorEl, isMenuOpen, menuClose, classes } = this.props;
+    return (
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+        open={isMenuOpen}
+        onClose={menuClose}
+      >
+        <MenuItem>
+          <ImageAvatars src={userA.image} className="user-pic" alt="" />
+          {userA.email}
+        </MenuItem>
+        <MenuItem onClick={menuClose}>
+          <NavLink to="/profile" exact className="nav-link">
+            <Button size="small" className={classes.margin}>
+              <FontAwesomeIcon icon="user" size="2x" />
+              &nbsp;My Account
+            </Button>
+          </NavLink>
+        </MenuItem>
+        <MenuItem onClick={menuClose}>
+          <SimpleDialogWrapped />
+        </MenuItem>
+        <MenuItem onClick={menuClose}>
+          <NavLink to="/logout" exact className="nav-link">
+            <Button size="small" className={classes.margin}>
+              <FontAwesomeIcon icon="sign-out-alt" size="2x" />
+              &nbsp;Logout
+            </Button>
+          </NavLink>
+        </MenuItem>
+      </Menu>
+    );
+  }
 }
 
 /*
@@ -71,4 +58,4 @@ UserMenuItems.propTypes = {
   }
   */
 
-export default UserMenuItems                    
+export default UserMenuItems;
